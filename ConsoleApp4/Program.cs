1cc4b375a15a9a74b84e7865295704679c76459c
Keli06001 <keli06001@utb.vaxjo.se>
2024-04-10 10:03:10 +0200

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Person
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    public string FullName
+    {
+        get { return FirstName + " " + LastName; }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Person person = new Person();
+
+        person.FirstName = "John";
+        person.LastName = "Doe";
+
+        Console.WriteLine("Full Name: " + person.FullName);
+    }
+}
